de48c7be69962149051df42ed5fb7e566ddbcfbc
Ankita Patil <patilankita79@gmail.com>
2018-02-01 22:44:20 -0600

    State Pattern

---
+++ State Pattern/app.js
@@ -0,0 +1,78 @@
+const PageState = function() {
+  let currentState = new homeState(this);
+
+  this.init = function() {
+    this.change(new homeState);
+  }
+
+  this.change = function(state) {
+    currentState = state;
+  }
+};
+
+// Home State
+const homeState = function(page) {
+  document.querySelector('#heading').textContent = null;
+  document.querySelector('#content').innerHTML = `
+  <div class="jumbotron">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+ </div>
+  
+  `;
+}
+
+// About State
+const aboutState = function(page) {
+  document.querySelector('#heading').textContent = 'About Us';
+  document.querySelector('#content').innerHTML = `
+    <p>This is the about page</p>
+  `;
+};
+
+
+// Contact State
+const contactState = function(page) {
+  document.querySelector('#heading').textContent = 'Contact Us';
+  document.querySelector('#content').innerHTML = `
+    <p>This is the contact page</p>
+  `;
+};
+
+// Instantiate the page state
+const page = new PageState();
+
+// Init the first state
+page.init();
+
+// UI variables
+const home = document.getElementById('home'),
+      about = document.getElementById('about'),
+      contact = document.getElementById('contact');
+
+
+// Home
+home.addEventListener('click', (e) => {
+  page.change(new homeState);
+
+  e.preventDefault();
+});
+
+// About
+about.addEventListener('click', (e) => {
+  page.change(new aboutState);
+
+  e.preventDefault();
+});
+
+// Contact
+contact.addEventListener('click', (e) => {
+  page.change(new contactState);
+
+  e.preventDefault();
+})
